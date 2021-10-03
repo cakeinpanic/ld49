cf268d97464917fc90c7010d3baff1e9ba58c071
cakeinpanic <cakeinpanic@gmail.com>
2021-10-03 14:25:24 +0300

    add color animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import lamp from './lamp.png';
-import light from './light.png';
-import './App.css';
+import './App.css'
+import lamp from './lamp.png'
+import light from './light.png'
 
-function App() {
-  return (
-     <div className="Game">
-       <img className="img light" src={light}/>
-       <img className="img lamp" src={lamp}/>
-
-     </div>
-  );
+function getRandomBetween(min: number, max: number) {
+  return Math.random() * (max - min) + min
 }
 
-export default App;
+function changeColor(timeout: number) {
+  setTimeout(() => {
+    const root = document.documentElement
+
+    root.style.setProperty('--brightness', getRandomBetween(70, 120) + '%')
+    root.style.setProperty('--hue', getRandomBetween(0, 180) + 'deg')
+    root.style.setProperty('--saturate', getRandomBetween(70, 120) + '%')
+    changeColor(getRandomBetween(1000, 3000))
+  }, timeout)
+}
+
+function App() {
+  changeColor(0)
+  return (
+    <div className="Game">
+      <img className="img light" src={light}/>
+      <img className="img lamp" src={lamp}/>
+    </div>
+  )
+}
+
+export default App
