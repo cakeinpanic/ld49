b7d13e2e90e457910e623c5e2358d9725abd54dd
cakeinpanic <cakeinpanic@gmail.com>
2021-10-04 22:16:22 +0300

    make answers squiggle

--- src/Doors.tsx
+++ src/Doors.tsx
@@ -21,30 +21,7 @@ const [isStarted, setIsStarted] =useState(false)
           <div className='right'></div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'none' }}>
-        <defs>
-          <filter id="squiggly-0">
-            <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="0"/>
-            <feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="2"/>
-          </filter>
-          <filter id="squiggly-1">
-            <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="1"/>
-            <feDisplacementMap in="SourceGraphic" in2="noise" scale="3"/>
-          </filter>
-          <filter id="squiggly-2">
-            <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="2"/>
-            <feDisplacementMap in="SourceGraphic" in2="noise" scale="2"/>
-          </filter>
-          <filter id="squiggly-3">
-            <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="3"/>
-            <feDisplacementMap in="SourceGraphic" in2="noise" scale="3"/>
-          </filter>
-          <filter id="squiggly-4">
-            <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="4"/>
-            <feDisplacementMap in="SourceGraphic" in2="noise" scale="1"/>
-          </filter>
-        </defs>
-      </svg>
+
 
     </div>
       <button className="welcome-btn  btn"  style={{opacity: isStarted ? '0':'100%', bottom: isStarted ? '20px': '60px'}}>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,30 @@ ReactDOM.render(
   <React.StrictMode>
     <link href="https://fonts.googleapis.com/css?family=Baloo+2:400,800" rel="stylesheet"/>
     <App/>
+    <svg xmlns="http://www.w3.org/2000/svg" version="1.1" style={{ display: 'none' }}>
+      <defs>
+        <filter id="squiggly-0">
+          <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="0"/>
+          <feDisplacementMap id="displacement" in="SourceGraphic" in2="noise" scale="2"/>
+        </filter>
+        <filter id="squiggly-1">
+          <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="1"/>
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="3"/>
+        </filter>
+        <filter id="squiggly-2">
+          <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="2"/>
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="2"/>
+        </filter>
+        <filter id="squiggly-3">
+          <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="3"/>
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="3"/>
+        </filter>
+        <filter id="squiggly-4">
+          <feTurbulence id="turbulence" baseFrequency="0.01" numOctaves="3" result="noise" seed="4"/>
+          <feDisplacementMap in="SourceGraphic" in2="noise" scale="1"/>
+        </filter>
+      </defs>
+    </svg>
   </React.StrictMode>,
   document.getElementById('root')
 );
