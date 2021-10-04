0cc843b22c41bf9a43ed3b9e5a53c1b34fd30d79
cakeinpanic <cakeinpanic@gmail.com>
2021-10-04 17:36:33 +0300

    use blinking scenarios

--- src/hooks/lightBlink.hook.ts
+++ src/hooks/lightBlink.hook.ts
@@ -1,44 +1,55 @@
-import { useCallback, useEffect, useRef } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import { eLampState } from '../lampState.enum'
-import { getRandomBetween } from './hook.utils'
+import {
+  NEUTRAL_BLINK_SCENARIO,
+  NIGTHMARE_BLINK_SCENARIO,
+  NO_BLINK_SCENARIO,
+  SAD_BLINK_SCENARIO
+} from './lightScenarios'
 
 export const useLightBlink = (lampState: eLampState) => {
 
   const blinkTimeoutRef = useRef<any>()
+  const [scenario, setScenario] = useState(SAD_BLINK_SCENARIO)
+  const [nextStep, setNextStep] = useState(0)
 
   const blink = useCallback(() => {
     const root = document.body
-    let withoutLight = [100, 200]
-    let nextBlink = [1000, 2000]
+    clearTimeout(blinkTimeoutRef.current)
+    root?.style.setProperty('--opacity', scenario[nextStep].opacity + '%')
+    console.log('blink', scenario[nextStep])
+    blinkTimeoutRef.current = setTimeout(() => {
+      setNextStep((nextStep + 1) % scenario.length)
+    }, scenario[nextStep].time)
+  }, [lampState, scenario, nextStep])
+
+  useEffect(() => {
+    let scenario: any
     switch (lampState) {
-      case eLampState.nightmare:
-        withoutLight = [100, 1500]
-        nextBlink = [100, 500]
-        break
-      case eLampState.sad:
-        withoutLight = [200, 500]
-        nextBlink = [1000, 4000]
+      case eLampState.happy:
+      case eLampState.ok:
+        scenario = NO_BLINK_SCENARIO
         break
       case eLampState.neutral:
-        withoutLight = [100, 200]
-        nextBlink = [5000, 8000]
+        scenario = NEUTRAL_BLINK_SCENARIO
+        break
+      case eLampState.sad:
+        scenario = SAD_BLINK_SCENARIO
+        break
+      case eLampState.nightmare:
+        scenario = NIGTHMARE_BLINK_SCENARIO
         break
     }
-
-    clearTimeout(blinkTimeoutRef.current)
-    if (lampState === eLampState.nightmare || lampState === eLampState.sad || lampState === eLampState.neutral) {
-      root?.style.setProperty('--opacity', '0%')
+    setScenario(scenario)
+    if (nextStep % 2) {
+      setNextStep(1)
+    } else {
+      setNextStep(0)
     }
-    setTimeout(() => {
-      root?.style.setProperty('--opacity', '100%')
-      blinkTimeoutRef.current = setTimeout(() => {
-        blink()
-      }, getRandomBetween(nextBlink))
-    }, getRandomBetween(withoutLight))
   }, [lampState])
 
   useEffect(() => {
     blink()
-  }, [lampState, blink])
+  }, [blink])
 
 }
--- src/hooks/lightScenarios.ts
+++ src/hooks/lightScenarios.ts
@@ -8,5 +8,30 @@ export const NIGTHMARE_BLINK_SCENARIO = [
   {opacity: 0, time: 200},
   {opacity: 100, time: 200},
   {opacity: 0, time: 500},
-  {opacity: 100, time: 3000},
+  {opacity: 100, time: 5000},
 ]
+export const SAD_BLINK_SCENARIO = [
+  {opacity: 0, time: 100},
+  {opacity: 100, time: 1000},
+  {opacity: 0, time: 300},
+  {opacity: 100, time: 100},
+  {opacity: 0, time: 100},
+  {opacity: 100, time: 1000},
+  {opacity: 0, time: 100},
+  {opacity: 100, time: 1500},
+  {opacity: 0, time: 200},
+  {opacity: 100, time: 2000},
+]
+export const NEUTRAL_BLINK_SCENARIO = [
+  {opacity: 0, time: 100},
+  {opacity: 100, time: 2000},
+  {opacity: 0, time: 100},
+  {opacity: 100, time: 1000}
+]
+
+export const NO_BLINK_SCENARIO = [
+  {opacity: 100, time: 5000},
+  {opacity: 100, time: 5000}
+]
+
+
