fa78e51437a6bd50bdee246ad591deb39e0d3afe
cakeinpanic <cakeinpanic@gmail.com>
2021-10-05 14:08:41 +0300

    fix font blinking

--- src/Doors.tsx
+++ src/Doors.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Doors.css'
 
 export const Doors = ({ onStart, allIsLoaded }: { allIsLoaded: boolean, onStart: Function }) => {
   const [isStarted, setIsStarted] = useState(false)
   const [showAbout, setShowAbout] = useState(false)
+
   const start = () => {
     const root = document.body
     root?.style.setProperty('--anim', 'infinite')
@@ -11,25 +12,31 @@ export const Doors = ({ onStart, allIsLoaded }: { allIsLoaded: boolean, onStart:
     setIsStarted(true)
   }
 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAbout(true)
+    }, 10)
+  }, [])
+
   return (<>
       <div className="doors-container">
-        <div className='door' onClick={()=> allIsLoaded && start()}>
+        <div className='door' onClick={() => allIsLoaded && start()}>
           <div className='face'>
             <button style={{ opacity: allIsLoaded ? '1' : '0' }} className="loading-btn btn--stripe btn">
               Start
             </button>
 
-            <div style={{opacity: !allIsLoaded ? '1':'0'}}className="running-stripe preloader"/>
+            <div style={{ opacity: !allIsLoaded ? '1' : '0' }} className="running-stripe preloader"/>
 
-          <div className='right'/>
+            <div className='right'/>
+          </div>
         </div>
-      </div>
 
 
       </div>
       {/*<div className="about" onClick={()=>{setShowAbout(true)}}>About</div>*/}
       <button className="welcome-btn  btn"
-              style={{ display: showAbout ? 'block': undefined ,opacity: isStarted ? '0' : '1', bottom: isStarted ? '20px' : '60px' }}>
+              style={{ opacity: isStarted || !showAbout ? '0' : '1', bottom: isStarted ? '20px' : '60px' }}>
         Hi, this is a small cozy game about unstable emotions and things that surround us.<br/><br/> It was designed to
         be played with sound turned on, moreover sound makes the gamplay whole. <br/> Please turn it on ❤️
         <br/><br/>
